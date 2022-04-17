ad09b5bce2995f7d027a2c20106f6e5ed900c166
vargur <russiankickingbear@gmail.com>
2022-04-18 00:50:19 +0300

    Add forgotten file

---
+++ RecipesService.Handlers/Recipes/GetRecipes/GetRecipesResponse.cs
@@ -0,0 +1,19 @@
+﻿using RecipesService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace RecipesService.Handlers.Recipes.GetRecipes
+{
+    public sealed class GetRecipesResponse
+    {
+        public Guid UniqueId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Directions { get; set; }
+
+        public IList<string> Categories { get; set; }
+
+        public IList<RecipePart> RecipeParts { get; set; }
+    }
+}
